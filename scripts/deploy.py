4173b28020b6f75decf162755bca6c9ff75a4ec8
Lemmynjash <lemmynjariakiarie@gmail.com>
2022-01-19 12:54:30 +0300

    did a ganache-local setup

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,24 @@
-from brownie import FundMe
+from brownie import FundMe, network, config, MockV3Aggregator
 import scripts.helpful_scripts as hp
 
 
 def deploy_fund_me():
     account = hp.getaccounts()
-    fund_me = FundMe.deploy({"from": account}, publish_source=True)
+
+    # if we are on a persistent network like rinkeby,use the associated address
+    # otherwise deploy mocks(this is a deployed fake version for interaction)
+    if network.show_active() not in hp.LOCAL_BLOCHAIN_ENVIRONMENT:
+        price_feed_address = config["networks"][network.show_active(
+        )]["eth_usd_price_list"]
+    else:
+        print(f" The active netork is {network.show_active()}")
+        hp.deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+        print("Mocks Deployed.....")
+
+    fund_me = FundMe.deploy(price_feed_address,
+                            {"from": account},
+                            publish_source=config["networks"][network.show_active()].get("verify"))
     print(f"Contract deployed to {fund_me.address}")
 
 
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,8 +1,20 @@
-from brownie import network, accounts
+from brownie import network, accounts, MockV3Aggregator
+from web3 import Web3
+
+DECIMALS = 18
+STARTING_PRICE = 2000
+LOCAL_BLOCHAIN_ENVIRONMENT = ["development", "ganache-local"]
 
 
 def getaccounts():
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCHAIN_ENVIRONMENT:
         return accounts[0]
     else:
         return accounts.load("lemmy-account")
+
+
+def deploy_mocks():
+    print("Deploying Mocks.....")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": getaccounts()})
